6a4f936c6b33feaa00285349f5db9fd4c2261653
matscoder <levin.tsu@outlook.com>
2022-07-19 18:09:23 +0800

    day1  P7

---
+++ 7,13.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <stdio.h>;
+//此处一定要写头文件
+//int main()
+//{
+//	int age = 20;
+//	double weight = 75.3;
+//	age = age + 1;
+//	weight = weight - 10;
+//	printf ("%d\n", age);
+//	printf("%lf\n", weight);
+//	return 0;
+//}
+
+
+
+
+//int a = 100;
+//int main()
+
+//{
+//	int a = 10;
+//		printf("%d\n", a);
+//		return 0;
+//}
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d,%d", &a, &b);
+	sum = a + b;
+		printf("sum=%d\n", sum);
+	return 0;
+}
